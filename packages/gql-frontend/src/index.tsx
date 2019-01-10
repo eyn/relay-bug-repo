9d506a876c76aeb669f5ec1129cf2355c91c82a6
James Moore <james.moore@cantab.net>
2019-01-10 16:56:16 +0000

    initial import

---
+++ packages/gql-frontend/src/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+} from 'relay-runtime';
+
+function fetchQuery(
+    operation: any,
+    variables: any,
+) {
+    return fetch('/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables,
+        }),
+    }).then(response => {
+        return response.json();
+    });
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
